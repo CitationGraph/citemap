3de4d21807c60b7d2770f09f49d5c8261c274132
Steven Kotwal <7886355+panda@users.noreply.github.com>
2024-12-03 08:21:13 -0500

    Update folder structure for Vercel

--- backend/bin/server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PATH="$HOME/.local/bin:$PATH"
-
-# Run flask server
-# Usage: ./server.sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR/..
-poetry run flask run
